3ad25027dbde045b93c0403a7b3408a7246696db
Ian Dai <zzxx6152@gmail.com>
2019-04-14 18:56:33 +0800

    Third Commit

--- app/src/main/java/jaiyou/nfu/chiayitravel/HotelListActivity.kt
+++ app/src/main/java/jaiyou/nfu/chiayitravel/HotelListActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 
 class HotelListActivity : AppCompatActivity() {
 
-    private var respData: String = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotel_list)
